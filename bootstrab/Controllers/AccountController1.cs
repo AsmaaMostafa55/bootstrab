6cd2b8ff8ca655b61dffc5361c5602dcc2617283
DELL <DELL@DESKTOP-COP5MC3>
2024-09-10 15:48:03 +0300

    Add project files.

---
+++ bootstrab/Controllers/AccountController1.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace bootstrab.Controllers
+{
+    public class AccountController1 : Controller
+    {
+        public IActionResult Login()
+        {
+            return View();
+        }
+
+        public IActionResult Register()
+        {
+            return View();
+        }
+
+
+
+
+
+    }
+}
---
+++ bootstrab/Controllers/HomeController.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace bootstrab.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult AboutUs()
+        {
+            return View();
+        }
+
+        public IActionResult Contact()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+
+    }
+}
